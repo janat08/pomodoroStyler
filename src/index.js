6f24a130a3ffb727ff03fbd9ee7b192dd6b69be2
janat08 <jey.and.key@gmail.com>
2019-07-28 11:23:28 +0600

    date/keys

--- src/index.js
+++ src/index.js
@@ -9,24 +9,28 @@ import obs from 'store/plugins/observe'
 import { localStored } from 'mobx-stored'
 import _ from 'lodash'
 
+function log (...args){
+  console.log(...args)
+}
+
+
 store.addPlugin(events)
 store.addPlugin(expire)
 store.addPlugin(obs)
 let c = store //cache
 window.store = store
-// const primary = c.get("primary")
-// if (primary){
-//   var obsId = c.observe('s', function(val, oldVal) {
-//     Object.assign(s, val)
-//   })
-//   var obsId2 = c.observe('primary', function(val, oldVal) {
-//     if (val = false){
-//       Object.assign(s, newState)
-//     }
-//   })
-// }
 
 function notify(title, opts){
+  // Push.create("Hello world!", {
+//   body: "How's it hangin'?",
+//   icon: '/icon.png',
+//   timeout: 9000,
+//   onClick: function () {
+//       window.focus();
+//       this.close();
+//   }
+// });
+
   if (s.s.notifications){
     Push(title, opts)
   }
@@ -36,18 +40,26 @@ function notify(title, opts){
 function setc(key, val, duration = 1000){
   store.set(key,val, new Date().getTime()+duration) //miliseconds to cache for
 }
-const old = c.get("save")
+// const old = c.get("save")
+let active = c.get("active")
 let id = c.get("id") || 0
 id +=1
 c.set("id", id)
+const verId = 12
+if (!ver()){c.set("active", 1); c.set("id", 1); log('set')}
+function ver (){
+  const save = store.get("save")
+  return save.ver == verId
+
+}
 const defaultState = {
-  active: 0,
-  id: id,
-  ver: 1,
+  // active: active? 1,
+  active: ver()? active: 1,
+  id: ver()? id : 1,
+  ver: verId,
   start: true,
   count: 0,
   timer: 20, //60*25
-  primary: true,
   s: {
     notifications: true
   }, 
@@ -59,17 +71,13 @@ const defaultState = {
   },
   get names () {
     return this.name +234
-  }
+  },
+  get primary () {return this.active == this.id}
 }
 const s = observable(defaultState)
 window.s = s
 const save = toJS(s)
-if (c.get("save") && save.ver == old.ver){
-  Object.assign(s, old)
-} else {
-  c.set("save", save)
-}
-// const {start, count, timer, timeString} = s
+
 const actions = {
   tick() {
     const {count, timer} = s
@@ -91,40 +99,42 @@ const actions = {
   },
 }
 
-// Push.create("Hello world!", {
-//   body: "How's it hangin'?",
-//   icon: '/icon.png',
-//   timeout: 9000,
-//   onClick: function () {
-//       window.focus();
-//       this.close();
-//   }
-// });
-
 let act = {}
 let stamp
 function syncronize (func, a) {
   stamp = new Date()
   c.set("stateEvents", {ev: a, stamp: stamp})
-  c.set("save", toJS(s))
   return func
 }
 for (let a in actions){
   act[a] = action((...args)=>{
-    let res
-    if (a == "tick"){
-      res = _.throttle(syncronize(actions[a], a), 1000)(...args)
-    } else {
-      res = syncronize(actions[a], a)(...args)
+    syncronize(actions[a], a)
+    if (!s.primary){
+      return ()=>{}
     }
-    return res
+    return actions[a]
   })
 }
+var primaryOb = c.observe('active', val=>{
+  c.active = val
+})
+
 var obsId = c.observe('stateEvents', function(val, oldVal) {
   if (typeof val == "undefined") return
   if (val.stamp == stamp) return
-  console.log('running')
+  // console.log('running')
   act[val.ev]
+})
+var obsId = c.observe('save', function(val, oldVal) {
+  if (typeof val == "undefined") return
+  if (s.primary) return
+  if (save.ver == val.ver){
+    Object.assign(s, val, {active: active})
+  } else {
+    c.set("save", save)
+  }
+  // log("reassign")
+  // Object.assign(s, val)
 })
 window.act = act
 
@@ -139,8 +149,9 @@ autorun(function notificationPermissions(){
   }
 })
 
-autorun(()=>{
-  const {start} = s
+autorun(function tick(){
+  const {start, primary} = s
+  if (!primary) return
   let inter
   if (start){
     inter = setInterval(act.tick, 1000)
@@ -149,12 +160,24 @@ autorun(()=>{
     inter = null
   }
 })
+
+autorun(function tick(){
+  c.set("save", toJS(s))
+})
 // store.set("unloading", "window")
 
-window.onbeforeunload = function(){
+// window.onbeforeunload = function(){
+//   store.set("active", s.active+1)
+// }
+console.log(1, s.active)
+// store.set("active", s.active+1)
+console.log(2, store.get("active"))
+window.addEventListener('beforeunload', (event) => {
+  console.log(s.active)
   store.set("active", s.active+1)
-}
-
+  event.returnValue = '';
+  return
+});
 
 document.registerElement("my-profile", class extends hyperElement{
 
