5337de0af78650c88a1a89d86a3249a59ca9ada0
janat08 <jey.and.key@gmail.com>
2019-07-29 17:19:28 +0600

    builds

---
+++ static/sw.js
@@ -0,0 +1,40 @@
+
+var cacheName = 'v12';
+// var filesToCache = [
+//     '/',
+//     '/index.html',
+//     '/hello-world.css'
+// ];
+
+self.addEventListener('install', event => {
+  console.log('installing')
+    event.waitUntil(async function () {
+      const cache = await caches.open(CACHE_NAME)
+  
+    //   await cache.addAll(CACHED_URLS)
+    }())
+  })
+
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+});
+
+
+// self.addEventListener('fetch', function(event) {
+//   // if (new URL(event.request.url).hostname == "localhost"){
+//   //   console.log('quiting')
+//   //   return
+//   // }
+//     event.respondWith(
+//       caches.open('mysite-dynamic').then(function(cache) {
+//         return cache.match(event.request).then(function(response) {
+//           var fetchPromise = fetch(event.request).then(function(networkResponse) {
+//             cache.put(event.request, networkResponse.clone());
+//             return networkResponse;
+//           })
+//           return response || fetchPromise;
+//         })
+//       })
+//     );
+//   });
+  
